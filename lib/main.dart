25095eee6df3ccc82848b84b83a23ca43702cd5a
Moamen Mohamed <99367343+MoamenMohamed101@users.noreply.github.com>
2022-07-24 18:11:17 +0200

    actors

--- lib/main.dart
+++ lib/main.dart
@@ -22,55 +22,204 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            expandedHeight: 450,
-            backgroundColor: Colors.black,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage('assets/images/emma.jpg'),
-                      fit: BoxFit.cover),
-                ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.bottomRight,
-                      colors: [
-                        Colors.black,
-                        Colors.black.withOpacity(.3),
-                      ],
+      body: Stack(
+        children: [
+          CustomScrollView(
+            //physics: const BouncingScrollPhysics(),
+            slivers: [
+              SliverAppBar(
+                expandedHeight: 450,
+                backgroundColor: Colors.black,
+                flexibleSpace: FlexibleSpaceBar(
+                  collapseMode: CollapseMode.parallax,
+                  background: Container(
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/emma.jpg'),
+                          fit: BoxFit.cover),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.bottomRight,
+                          colors: [
+                            Colors.black,
+                            Colors.black.withOpacity(.3),
+                          ],
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            FadeAnimation(
+                              1,
+                              const Text(
+                                'Emma Watson',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: [
+                                FadeAnimation(
+                                  1.2,
+                                  const Text(
+                                    '60 videos',
+                                    style: TextStyle(
+                                        color: Colors.grey, fontSize: 16),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 50,
+                                ),
+                                FadeAnimation(
+                                  1.3,
+                                  const Text(
+                                    '240k Subscribes',
+                                    style: TextStyle(
+                                        color: Colors.grey, fontSize: 16),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        FadeAnimation(
-                          1,
-                          const Text(
-                            'Emma Watson',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 40,
-                                fontWeight: FontWeight.bold),
+                ),
+              ),
+              SliverList(
+                delegate: SliverChildListDelegate(
+                  [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          FadeAnimation(
+                            1.6,
+                            const Text(
+                              'Emma Charlotte Duerre Watson was born in Paris, France, to English parents, Jacqueline Luesby and Chris Watson, both lawyers. She moved to Oxfordshire when she was five, where she attended the Dragon School.',
+                              style: TextStyle(color: Colors.grey, height: 1.4),
+                            ),
                           ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        FadeAnimation(
-                          1.2,
-                          const Text(
-                            '60 videos',
-                            style: TextStyle(color: Colors.grey, fontSize: 16),
+                          const SizedBox(
+                            height: 40,
                           ),
-                        ),
-                      ],
+                          FadeAnimation(
+                            1.6,
+                            const Text(
+                              'Born',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          FadeAnimation(
+                            1.6,
+                            const Text(
+                              'April, 15th 1990, Paris, France',
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          FadeAnimation(
+                            1.6,
+                            const Text(
+                              'Nationality',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          FadeAnimation(
+                            1.6,
+                            const Text(
+                              'British',
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          FadeAnimation(
+                            1.6,
+                            const Text(
+                              'Videos',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          FadeAnimation(
+                            1.8,
+                            Container(
+                              height: 200,
+                              child: ListView(
+                                scrollDirection: Axis.horizontal,
+                                children: [
+                                  makeVideo('assets/images/emma-1.jpg'),
+                                  makeVideo('assets/images/emma-2.jpg'),
+                                  makeVideo('assets/images/emma-3.jpg'),
+                                ],
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 120,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Positioned.fill(
+            bottom: 40,
+            child: FadeAnimation(
+              2,
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  height: 50,
+                  margin: const EdgeInsets.symmetric(horizontal: 30),
+                  decoration: BoxDecoration(
+                    color: Colors.yellow[700],
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Align(
+                    child: Text(
+                      'Follow',
+                      style: TextStyle(color: Colors.white),
                     ),
                   ),
                 ),
@@ -78,6 +227,33 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  makeVideo(String image) {
+    return AspectRatio(
+      aspectRatio: 1.5 / 1,
+      child: Container(
+        margin: const EdgeInsets.only(right: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(image: AssetImage(image), fit: BoxFit.cover),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.bottomRight,
+              colors: [
+                Colors.black.withOpacity(.9),
+                Colors.black.withOpacity(.3),
+              ],
+            ),
+          ),
+          child: const Align(
+            child: Icon(Icons.play_arrow, color: Colors.white, size: 70),
+          ),
+        ),
       ),
     );
   }
